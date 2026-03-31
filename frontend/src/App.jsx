09e681983047a165e72a0bd2708bde4a98fc83ee
Sapna44Y <yadavsapna2004@gmail.com>
2026-03-31 19:31:38 +0530

    All fetcher complete

---
+++ frontend/src/App.jsx
@@ -0,0 +1,37 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Login from "./pages/Auth/Login";
+import SignUp from "./pages/Auth/signUp";
+import Home from "./pages/Home/Home";
+
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" exact element={<Root />} />
+          <Route path="/dashboard" exact element={<Home />} />
+          <Route path="/login" exact element={<Login />} />
+          <Route path="/signup" exact element={<SignUp />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
+
+const Root = () => {
+  //Check if token exists in localStorage
+  const isAuthenticated = !!localStorage.getItem("token");
+  //Redirect to dashboard if authenticated, otherwise to login
+  return isAuthenticated ? (
+    <Navigate to="/dashboard" />
+  ) : (
+    <Navigate to="/login" />
+  );
+};
+
+export default App;
---
+++ frontend/src/utils/helper.js
@@ -0,0 +1,40 @@
+import ADD_STORY_IMG from "../assets/images/add-story.svg";
+import NO_SEARCH_DATA_IMG from "../assets/images/no-search-data.svg";
+import NO_FILTER_DATA_IMG from "../assets/images/no-filter-data.svg";
+
+export const validateEmail = (email) => {
+  const rgex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return rgex.test(email);
+};
+
+export const getInitials = (name) => {
+  if (!name) return "";
+  const words = name.split(" ");
+  let initials = "";
+  for (let i = 0; i < Math.floor(words.length, 2); i++) {
+    initials += words[i][0];
+  }
+  return initials.toUpperCase();
+};
+
+export const getEmptyCardMessage = (filterType) => {
+  switch (filterType) {
+    case "search":
+      return "Oops! No stories found matching your search.";
+    case "date":
+      return `No stories found in the give date range`;
+    default:
+      return `Start creating your first Travel Story! click the 'Add' button to join down your thoughts, ideas, and memories. Let's get Started!`;
+  }
+};
+
+export const getEmptyCardImg = (filterType) => {
+  switch (filterType) {
+    case "search":
+      return NO_SEARCH_DATA_IMG;
+    case "date":
+      return NO_FILTER_DATA_IMG;
+    default:
+      return ADD_STORY_IMG;
+  }
+};
